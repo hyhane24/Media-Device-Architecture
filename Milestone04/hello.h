bc9ea4728fe6433b5b56f71af5ef1e7747843be6
Hane Yie <47871955+hyhane24@users.noreply.github.com>
2021-05-08 00:22:06 -0400

    milestone 4 part two

---
+++ Milestone04/hello.h
@@ -0,0 +1,5 @@
+// hello sound made by wav2c
+
+extern const unsigned int hello_sampleRate;
+extern const unsigned int hello_length;
+extern const signed char hello_data[];
---
+++ Milestone04/theme.h
@@ -0,0 +1,5 @@
+// theme sound made by wav2c
+
+extern const unsigned int theme_sampleRate;
+extern const unsigned int theme_length;
+extern const signed char theme_data[];
